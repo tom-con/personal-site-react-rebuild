156199b2e54546be1989214cac1bdac698a10db7
Thomas Conchie <tconchie@gmail.com>
2018-05-23 22:27:40 -0600

    Begins implementation of menu components: -MenuComponent - presentational, renders menu styles -MenuItemComponent - presentational, renders menu item styles -MenuContainer - container, controls menu components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import LayoutRoute from './Components/Layout/LayoutRoute'
+import Menu from './Components/Navbar/MenuContainer'
 
 class App extends Component {
   render() {
     return (
-      <h1>init</h1>
+      <Router>
+        <LayoutRoute 
+          exact path='/'
+          component={() => <span>Hi</span> }
+        />
+      </Router>
     );
   }
 }
